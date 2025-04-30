5d2e377bdffb8ec414d66675971896f8102d2d9f
Lingfeng <lingfeng.liu@163.com>
2025-04-30 15:12:14 +0800

    Initial commit

---
+++ app/src/main/java/io/issc/android_tutorial_kt/DemoActivity.kt
@@ -0,0 +1,100 @@
+package io.issc.android_tutorial_kt
+
+import android.app.Activity
+import android.graphics.BitmapFactory
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.view.View
+import android.widget.*
+import android.widget.CompoundButton.INVISIBLE
+import android.widget.CompoundButton.OnCheckedChangeListener
+import android.widget.SeekBar.OnSeekBarChangeListener
+import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import io.issc.android_tutorial_kt.databinding.ActivityDemoBinding
+
+class DemoActivity: Activity() {
+    lateinit var btn:Button
+    lateinit var binding: ActivityDemoBinding
+    var cnt = 0
+    lateinit var txt:TextView
+    lateinit var img:ImageButton
+    lateinit var switch:Switch
+    lateinit var optList:RadioGroup
+    lateinit var seekbar:SeekBar
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDemoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+//        cnt = binding.cnt
+        btn = binding.btnSelect
+
+        btn.setOnClickListener{
+            var c = binding.cnt ?: 0
+            c += 1
+            binding.cnt = c
+            txt.text = "" + c
+            Log.i("main", "cnt = " + c)
+        }
+
+        txt = binding.txtCnt
+        switch = binding.switchBtn
+        switch.setOnCheckedChangeListener{p0, checked->
+                if (checked) {
+                    Log.i("demoacti", "switch on")
+                    img.visibility = View.VISIBLE
+                }
+                else
+                {
+                    Log.i("demoacti", "switch off")
+                    img.visibility = View.INVISIBLE
+                }
+        }
+
+        optList = binding.optList
+
+        optList.setOnCheckedChangeListener { p0, btnId ->
+            if (btnId == R.id.opt_1) {
+
+            } else if (btnId == R.id.opt_2) {
+
+            } else if (btnId == R.id.opt_3) {
+
+            } else {
+                //DO nothing
+            }
+        }
+
+        seekbar = binding.seekbar
+
+        seekbar.setOnSeekBarChangeListener(object:OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+                TODO("Not yet implemented")
+            }
+
+        })
+
+        img = binding.img
+
+        var str = "hello"
+        var bytes = str.toByteArray()
+        img.setImageBitmap(BitmapFactory.decodeByteArray(bytes, 0, bytes.size))
+//        img.setImageResource(R.drawable.ic_launcher_background)
+
+        btn.setOnClickListener{
+            cnt ++
+        }
+    }
+}
---
+++ app/src/main/java/io/issc/android_tutorial_kt/MainActivity.kt
@@ -0,0 +1,47 @@
+package io.issc.android_tutorial_kt
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import io.issc.android_tutorial_kt.ui.theme.Android_tutorial_ktTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Android_tutorial_ktTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Android_tutorial_ktTheme {
+        Greeting("Android")
+    }
+}
