e4e1a52ed3e62957f2b28163a1ece2c407545100
Lingfeng <lingfeng.liu@163.com>
2025-05-07 15:21:55 +0800

    ADD coordinate demo

---
+++ app/src/main/java/io/issc/android_tutorial_kt/ListAdapter.kt
@@ -0,0 +1,106 @@
+package io.issc.android_tutorial_kt
+
+import android.animation.ObjectAnimator
+import android.util.Log
+import android.view.*
+import android.view.GestureDetector.OnGestureListener
+import android.view.View.OnTouchListener
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+
+class ListAdapter(val dataList: List<Contact>): RecyclerView.Adapter<ListAdapter.ListItemHolder>() {
+
+    interface OnOpListener {
+        fun onRemove(data:Contact)
+    }
+
+    var opListener:OnOpListener? = null
+
+    class ListItemHolder(itemView: View) :ViewHolder(itemView) {
+
+        var txtName = itemView.findViewById<TextView>(R.id.txt_name)
+        var txtNum = itemView.findViewById<TextView>(R.id.txt_num)
+        var btn = itemView.findViewById<ImageButton>(R.id.btn_remove)
+        var rootView = itemView.findViewById<ViewGroup>(R.id.container)
+        var gestureDetector: GestureDetector
+        lateinit var data:Contact
+
+        init {
+            gestureDetector = GestureDetector(rootView.context, object:OnGestureListener{
+                override fun onDown(p0: MotionEvent): Boolean {
+                    return false
+                }
+
+                override fun onShowPress(p0: MotionEvent) {
+                }
+
+                override fun onSingleTapUp(p0: MotionEvent): Boolean {
+                    return false
+                }
+
+                override fun onScroll(
+                    e1: MotionEvent?,
+                    e2: MotionEvent,
+                    distanceX: Float,
+                    distanceY: Float
+                ): Boolean {
+                    TODO("Not yet implemented")
+                    return true
+                }
+
+                override fun onLongPress(p0: MotionEvent) {
+                }
+
+                override fun onFling(
+                    e1: MotionEvent?,
+                    e2: MotionEvent,
+                    velocityX: Float,
+                    velocityY: Float
+                ): Boolean {
+                    if (velocityY > 10) {
+                        val anime = ObjectAnimator.ofFloat(rootView, "translationX", rootView.translationX, 0.0f)
+                        anime.duration= 200
+                        anime.start()
+                    }
+                    else if (velocityX < -10) {
+                        val anime = ObjectAnimator.ofFloat(rootView, "translationX", rootView.translationX, -btn.width.toFloat())
+                        anime.duration= 200
+                        anime.start()
+                    }
+                    return true
+                }
+
+            })
+
+            rootView.setOnTouchListener(object : OnTouchListener{
+                override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+                    event?.let { gestureDetector.onTouchEvent(it) }
+                    return true
+                }
+            })
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListItemHolder {
+        var v = LayoutInflater.from(parent.context).inflate(R.layout.item_list, parent, false)
+
+        return ListItemHolder(v)
+    }
+
+
+    override fun getItemCount(): Int {
+        return dataList.size
+    }
+
+    override fun onBindViewHolder(holder: ListItemHolder, position: Int) {
+        holder.txtName.text = dataList.get(position).name
+        holder.txtNum.text = dataList.get(position).num
+        holder.data = dataList.get(position)
+        holder.btn.setOnClickListener {
+            //TODO: position update
+            opListener?.onRemove(holder.data)
+        }
+    }
+}
